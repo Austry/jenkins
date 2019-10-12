29a2861be8f7f37ab5be165b52dd9a6605f8809d
austry <iliazasimov@gmail.com>
2019-10-12 12:57:48 +0300

    Update core/src/main/java/jenkins/model/Jenkins.java
    Co-Authored-By: Daniel Beck <1831569+daniel-beck@users.noreply.github.com>

--- core/src/main/java/jenkins/model/Jenkins.java
+++ core/src/main/java/jenkins/model/Jenkins.java
@@ -2750,6 +2750,7 @@ public class Jenkins extends AbstractCIBase implements DirectlyModifiableTopLeve
      * Returns quiet down reason if it was indicated.
      * @return
      *      Reason if it was indicated. null otherwise
+     *      @since TODO
      */
     @Exported
     @CheckForNull
